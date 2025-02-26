ae2135c6b0bea81fb709aa0fd9d8b94245c6e2e3
Team-Brisk <teambrisk.developer@gmail.com>
2025-02-26 12:10:43 +0700

    asdasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
                 fontSize: "2.25rem"
               }}
             >
-              สวัสดี, {user?.name || "Guest"}
+              sสวัสดี, {user?.name || "Guest"}
             </Typography>
 
           </motion.div>
